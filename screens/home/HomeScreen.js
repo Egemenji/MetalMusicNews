b067f8ca8dd0393d1b13b280187c1453301c98dc
yakintech <cagatayyildiz87@gmail.com>
2021-09-05 13:47:08 +0300

    cameraroll çalışmadı ama yükledik

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 import { Appearance } from 'react-native'
-
-
-
+import { RNCamera } from 'react-native-camera';
+import Contacts from 'react-native-contacts';
+import CameraRoll from "@react-native-community/cameraroll";
 
 const HomeScreen = () => {
 
     const [batteryLevel, setBatteryLevel] = useState(0);
     const [deviceId, setDeviceId] = useState('');
     const [totalMemory, settotalMemory] = useState(0);
-    const [isBatteryCharging, setisBatteryCharging] = useState(false)
+    const [isBatteryCharging, setisBatteryCharging] = useState(false);
+    const [contacts, setContacts] = useState([])
 
     useEffect(() => {
 
@@ -30,19 +31,32 @@ const HomeScreen = () => {
         });
 
         DeviceInfo.isBatteryCharging().then((isCharging) => {
-            
-            alert(isCharging)
+            isCharging ? setisBatteryCharging(1) : setisBatteryCharging(0)
 
-            setisBatteryCharging(isCharging)
+        });
 
-          });
+        Contacts.getAll().then(data => {
 
+            setContacts(data)
+
+
+            data.map(item => {
+                console.log(item.phoneNumbers[0].number);
+            })
+
+        })
+
+
+        // CameraRoll.getAlbums().then((data) => {
+        //     console.log('GALLERY',data);
+        // })
 
     }, [])
 
+
     return (
         <>
-            <View>
+            <ScrollView style={{ padding: 20 }}>
                 <Text>Battery Level: {batteryLevel}</Text>
                 <Text>Device Type: {deviceId}</Text>
                 <Text>Device Model: {DeviceInfo.getModel()}</Text>
@@ -50,8 +64,18 @@ const HomeScreen = () => {
                 <Text>Total Memory: {totalMemory}</Text>
                 <Text>is Battery Charging: {isBatteryCharging}</Text>
 
+                <View style={{ marginTop: 20 }}>
+                    <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Kişi Listesi</Text>
+                    {
+                        contacts.map((item) => {
+                            return (<Text style={{ marginTop: 5 }}>{item.givenName} {item.familyName}  / {item.phoneNumbers[0].number}</Text>)
+                        })
+                    }
+                </View>
 
-            </View>
+
+
+            </ScrollView>
         </>
 
     )
